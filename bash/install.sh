d74a3367cd6980820ba39c6f57ae366c1b43eca8
Purr Consuegra <purr.consuegra@solita.fi>
2019-05-13 17:21:25 +0300

    Bashrc

---
+++ bash/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+ln -si CURRENT_DIR/bashrc $HOME/.bashrc
+mkdir -p $HOME/.config/bash/init-scripts
+mkdir    $HOME/.config/bash/paths
+ln -si $HOME/.local/bin $HOME/.config/bash/paths/home-local-bin
