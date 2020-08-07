7ff181adfaec46caa5072163702056580d962e32
Purr Consuegra <david.consuegra@solita.fi>
2020-08-07 14:12:38 +0300

    Add bash customization role

--- ansible/prequisites.sh
+++ ansible/prequisites.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 sudo apt-get install ansible dialog
+ansible-galaxy install -r $CURRENT_DIR/requirements.yml
---
+++ ansible/roles/bash_customizations/files/bash_config/autorun.sh
@@ -0,0 +1,26 @@
+if [[ -z "${BASH_CONFIG}" ]]; then
+  echo "$0: $$BASH_CONFIG is not set!"
+  return
+fi
+
+export BASH_INIT_SCRIPTS=$BASH_CONFIG/init
+
+# iterate over our bashrc script files
+for script in $BASH_INIT_SCRIPTS/*.sh
+do
+    # check if the script is executable
+    if [ -x "${script}" ]; then
+        # run the script
+        source ${script}
+    fi
+done
+
+#Add stuff to the PATH variable
+for path in $BASH_CONFIG/paths/*
+do
+    # check if the path is a link
+    if [ -L "${path}" ]; then
+        # run the script
+        PATH="$PATH:$(readlink $path)"
+    fi
+done
