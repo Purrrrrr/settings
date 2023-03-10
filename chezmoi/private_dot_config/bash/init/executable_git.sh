403e05194d9e660751f94c564c1686d11fa8fb08
Purr Consuegra <purr.consuegra@solita.fi>
2023-03-10 15:24:47 +0200

    Made git ticket regex more easily configurable

--- chezmoi/private_dot_config/bash/init/executable_git.sh
+++ chezmoi/private_dot_config/bash/init/executable_git.sh
@@ -7,16 +7,20 @@ alias s='git status -s'
 alias lg="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias git-meld='git difftool  -d -t meld'
 
-#alias gc='git commit -m $(git rev-parse --abbrev-ref HEAD | egrep -o "[a-zA-Z]+-[0-9]+(/[a-zA-Z]+-[0-9]+)?") -e'
-function gc() {
+function current-ticket() {
   rev=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-  regex="[a-zA-Z]+-[0-9]+(/[a-zA-Z]+-[0-9]+)?"
-  ticket=$(egrep -o $regex <<< "$rev")
+  regex=$(cat ~/.config/bash/init/git-ticket-regex.txt)
+  grep -Po $regex <<< "$rev"
+}
+export -f current-ticket
+
+function gc() {
+  ticket=$(current-ticket)
 
   if [[ -z $ticket ]]; then 
     git commit "$@";
   else
-    git commit -m "$ticket" -e "$@";
+    git commit -m "$ticket": -e "$@";
   fi;
 }
 
