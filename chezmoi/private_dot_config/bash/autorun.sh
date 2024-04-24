8d907d3e42d385ab06172b17f2d13e4f0f311404
Purr Consuegra <purr.consuegra@solita.fi>
2024-04-24 10:42:33 +0300

    Fix jEnv config

--- chezmoi/private_dot_config/bash/autorun.sh
+++ chezmoi/private_dot_config/bash/autorun.sh
@@ -5,16 +5,6 @@ fi
 
 export BASH_INIT_SCRIPTS=$BASH_CONFIG/init
 
-# iterate over our bashrc script files
-for script in $BASH_INIT_SCRIPTS/*.sh
-do
-    # check if the script is executable
-    if [ -x "${script}" ]; then
-        # run the script
-        source ${script}
-    fi
-done
-
 #Add stuff to the PATH variable
 for path in $BASH_CONFIG/paths/*
 do
@@ -22,5 +12,15 @@ do
     if [ -L "${path}" ]; then
         # run the script
         PATH="$PATH:$(readlink $path)"
+    fi
+done
+
+# iterate over our bashrc script files
+for script in $BASH_INIT_SCRIPTS/*.sh
+do
+    # check if the script is executable
+    if [ -x "${script}" ]; then
+        # run the script
+        source ${script}
     fi
 done
